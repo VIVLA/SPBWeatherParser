dec01f814ac8e9460ab4dcf6c520e5829d0a6284
Alexander <43985010+VIVLA@users.noreply.github.com>
2020-11-05 11:50:48 +0300

    Add files via upload

---
+++ src/main/java/com/ldeng/spbweather/Wheather.java
@@ -0,0 +1,13 @@
+
+package com.ldeng.spbweather;
+
+import javax.swing.SwingUtilities;
+
+public class Wheather {
+    
+    public static void main(String[] args) {
+       Window win = new Window();
+       SwingUtilities.invokeLater(win);
+    }
+    
+}
---
+++ src/main/java/com/ldeng/spbweather/Window.java
@@ -0,0 +1,62 @@
+
+package com.ldeng.spbweather;
+
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class Window extends JFrame implements Runnable {
+    
+    private JTable table;
+    private DefaultTableModel model;
+
+    private JScrollPane scroll;
+    
+    @Override
+    public void run() {
+        try {
+            initFrame();
+        } catch (Exception ex) {
+            Logger.getLogger(Window.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void initFrame() throws Exception {
+        table = new JTable();
+        model = new DefaultTableModel();
+
+        String headers[] = {"Погода в Санкт-Петербурге"};
+   
+        model.setColumnIdentifiers(headers);   
+        table.setModel(model);
+        scroll = new JScrollPane(table);
+
+        insert();
+            
+        add(scroll, BorderLayout.CENTER);
+        setTitle("Weather");
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void insert() throws Exception {
+        Parser p = new Parser();        
+        ArrayList<String> data = p.getArray();
+
+        ArrayList<String> ar = new ArrayList<>();
+        for (int i = 0; i < data.size(); i++) {
+          ar.add(data.get(i));
+        }
+        for (int i = 0; i < ar.size(); i++) {
+          model.addRow(new Object[] { String.valueOf(ar.get(i)) });
+        }
+    }
+        
+}
